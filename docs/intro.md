c1795ed2a85f117d5eceb600ced2d2ac090fea93
黄博远 <david.2003@foxmail.com>
2023-11-10 17:35:33 +0800

    bugfix： 修复打包找不到/docs/intro路径

---
+++ docs/intro.md
@@ -0,0 +1,5 @@
+---
+hide_title: true
+title: intro
+---
+## Welcome, 记录重要的知识，有了缓存才能高效输出！！
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: "David's Site",
+  tagline: '看一看吧',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
